75b31b6088336041b77778004e85c2cb736dcdcc
Yamenko <yamenko@yandex.ru>
2021-11-16 23:41:39 +0300

    Make first function 'DRAW'

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+static void Draw(byte x, byte y, char simb) 
+{
+    Console.SetCursorPosition(x, y);
+    Console.Write(simb);
+}
 
+byte x1 = 1;
+byte y1 = 3;
+char simb1 = '*';
+Draw(x1, y1, simb1);
+
+byte x2 = 4;
+byte y2 = 5;
+char simb2 = '#';
+Draw(x2, y2, simb2);
+
+
+
+Console.ReadLine();
