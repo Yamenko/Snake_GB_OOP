5544f2a5f9c4abb5278229bf0ed918cc1050123c
Yamenko <yamenko@yandex.ru>
2021-11-17 21:46:17 +0300

    Incapsule parametrs in Point

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -8,9 +8,16 @@ namespace Snake
 {
     internal class Point
     {
+        
         public byte x;
         public byte y;
         public char simb;
+        public Point (byte _x, byte _y, char _simb)
+        {
+            x = _x;
+            y = _y;
+            simb = _simb;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,15 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Snake.Point p1 = new Snake.Point();
-p1.x = 1;
-p1.y = 3;
-p1.simb = '*';
+Snake.Point p1 = new Snake.Point(1, 3, '*');
 p1.Draw();
 
-Snake.Point p2 = new Snake.Point();
-p2.x = 4;
-p2.y = 5;
-p2.simb = '#';
+Snake.Point p2 = new Snake.Point(4, 5, '#');
 p2.Draw();
 
 
