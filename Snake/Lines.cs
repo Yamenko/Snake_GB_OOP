48304292acbf0edb535395184fb1df5d064d333f
Yamenko <yamenko@yandex.ru>
2021-11-27 22:36:05 +0300

    make a new boarding, del horizont & vertical

---
+++ Snake/Lines.cs
@@ -0,0 +1,26 @@
+﻿namespace Snake
+{
+    internal class Lines : Figura
+    {
+        public Lines(byte xStart, byte xStop, byte yStart, byte yStop, Char simb)
+        {
+            pLineList = new List<Point>();
+            Point p = new Point(xStart, yStart, simb);
+            for (byte i = xStart; i <= xStop; i++)
+            {
+                p = new Point(i, yStart, simb);
+                pLineList.Add(p);
+                p = new Point(i, yStop, simb);
+                pLineList.Add(p);
+            }
+            for (byte i = yStart; i <= yStop; i++)
+            {
+                p = new Point(xStart, i, simb);
+                pLineList.Add(p);
+                p = new Point(xStop, i, simb);
+                pLineList.Add(p);
+            }
+        }
+
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,12 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Snake;
+﻿using Snake;
 
 //Границы
-HorizontLine uLine = new HorizontLine(1, 99, 1, '+');
-HorizontLine dLine = new HorizontLine(1, 99, 25, '+');
-VerticalLine lLine = new VerticalLine(1, 25, 1, '+');
-VerticalLine rLine = new VerticalLine(1, 25, 99, '+');
-rLine.Draw(); uLine.Draw(); dLine.Draw(); lLine.Draw();
+Lines board = new Lines(1, 99, 1, 25, '0');
+board.Draw();
+
 
 //Змейка
 Point p = new Point(4, 5, '*');
@@ -16,7 +13,7 @@ snake.Draw();
 
 while (true)
 {
-    if (Console.KeyAvailable) 
+    if (Console.KeyAvailable)
     {
         snake.HandleKey(Console.ReadKey());
     }
